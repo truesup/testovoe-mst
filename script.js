3c830ad5a31d2773c53a057689683769ec94bf6d
Pavel Merekin <merekin33@gmail.com>
2025-01-25 14:13:24 +0700

    form logic added

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 const header = document.querySelector('.header');
 const burgerMenu = document.getElementById('burger-menu');
-const menuList = document.querySelector('.header-menu-list');
-const menuLinks = document.querySelectorAll('.header-menu-link');
+const headerMenuList = document.querySelector('.header-menu-list');
+const footerMenuList = document.querySelector('.footer-menu-list');
 
-menuList.addEventListener('click', event => {
-  if (event.target.classList.contains('header-menu-link')) {
-    document.querySelector('.is-current')?.classList.remove('is-current');
-    event.target.classList.add('is-current');
+function syncMenuSelection(target) {
+  document
+    .querySelectorAll('.is-current')
+    .forEach(link => link.classList.remove('is-current'));
 
-    if (window.innerWidth <= 760) {
-      burgerMenu.classList.remove('active');
-      menuList.classList.remove('active');
+  const targetHref = target.getAttribute('href');
+  document
+    .querySelectorAll(`a[href="${targetHref}"]`)
+    .forEach(link => link.classList.add('is-current'));
+}
+
+[headerMenuList, footerMenuList].forEach(menu => {
+  menu.addEventListener('click', event => {
+    if (event.target.tagName === 'A') {
+      syncMenuSelection(event.target);
+
+      if (window.innerWidth <= 760) {
+        burgerMenu.classList.remove('active');
+        headerMenuList.classList.remove('active');
+      }
     }
-  }
+  });
 });
 
 window.addEventListener('scroll', () => {
   header.classList.toggle('header-expanded', window.scrollY > 0);
 });
 
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('active');
-  menuList.classList.toggle('active');
-});
+// form
+document
+  .querySelector('.form-button')
+  .addEventListener('click', function (event) {
+    const form = document.querySelector('.contact-form');
+    const name = form.querySelector('input[type="text"]');
+    const email = form.querySelector('input[type="email"]');
+    const privacy = form.querySelector('input[type="checkbox"]');
+
+    if (!name.value || !email.value || !privacy.checked) {
+      alert(
+        'Please fill out all fields and accept the privacy policy before submitting!'
+      );
+      return;
+    }
+
+    if (!name.checkValidity() || !email.checkValidity()) {
+      alert('Please make sure all fields are filled correctly!');
+    }
+
+    alert('Form is correctly filled out and sent!');
+  });
